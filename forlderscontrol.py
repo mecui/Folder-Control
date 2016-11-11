665b784c9cadbd6b4a8e55398a9920c4902d146e
Chris Mengyao Cui <mcui3@hawk.iit.edu>
2016-11-11 15:37:30 -0600

    Add comments

--- forlderscontrol.py
+++ forlderscontrol.py
@@ -16,8 +16,12 @@ import shutil
 for ff in range(1,25):  # base on the number of folders. 
   for cc in range(1,11): # base on the number that files you want to move
     path_i = "c:\users\mengyaoc\desktop\Camera\out_Door"   # Here is the directory which you keep your files. 
-    path_f= os.path.join(path_i,"Scene"+str(ff))
-    fname = os.listdir(path_i)[1]
+    path_f= os.path.join(path_i,"Scene"+str(ff))   # get the name of sub-directories
+    fname = os.listdir(path_i)[1]                  # get the name of files 
+
+    #WARNING: IT WILL ALWAY TAKE THE FIRST FILE, AFTER YOU MOVE THE FIRST FILE, 
+    #THE SECOND WILL BE THE FIRST ONE SO YOU SHOULD ALWAYS KEEP MOVE OUT THE FIRST ONE
+    
     before = os.path.join(path_i,fname)
     after = os.path.join(path_f,fname)
     shutil.move(before, after)

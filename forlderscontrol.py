350e426d814ebea30ceaf116bdb64ef14d75322f
Chris Mengyao Cui <mcui3@hawk.iit.edu>
2016-11-14 11:46:36 -0600

    Update forlderscontrol.py
    Change variable names to something more sensible.

--- forlderscontrol.py
+++ forlderscontrol.py
@@ -13,10 +13,10 @@ import shutil
 #for i in range(1):
 #os.mkdir("Scene"+str[])
 
-for ff in range(1,25):  # base on the number of folders. 
-  for cc in range(1,11): # base on the number that files you want to move
+for folders_Count in range(1,25):  # base on the number of folders. 
+  for files_Move in range(1,11): # base on the number that files you want to move
     path_i = "c:\users\mengyaoc\desktop\Camera\out_Door"   # Here is the directory which you keep your files. 
-    path_f= os.path.join(path_i,"Scene"+str(ff))   # get the name of sub-directories
+    path_f= os.path.join(path_i,"Scene"+str(folders_Count))   # get the name of sub-directories
     fname = os.listdir(path_i)[1]                  # get the name of files 
 
     #WARNING: IT WILL ALWAY TAKE THE FIRST FILE, AFTER YOU MOVE THE FIRST FILE, 
@@ -25,7 +25,7 @@ for ff in range(1,25):  # base on the number of folders.
     before = os.path.join(path_i,fname)
     after = os.path.join(path_f,fname)
     shutil.move(before, after)
-    cc = cc+1
-  ff = ff + 1 
+    files_Move = files_Move + 1
+  folders_Count = folders_Count + 1 
 
 
